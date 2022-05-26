7f51d6df88e8ddfabbcd6583cafea7e9ccec686e
a-barinov <alex.barinov@gmail.com>
2022-05-26 20:09:34 +0200

    Minor fixes to audio qube install script

--- 7.Audio/install.sh
+++ 7.Audio/install.sh
@@ -33,17 +33,15 @@ message "CONFIGURING ${YELLOW}dom0"
 push_files "dom0"
 add_line dom0 "/etc/qubes-rpc/policy/liteqube.Message" "${VM_AUDIO} dom0 allow"
 add_line dom0 "/etc/qubes-rpc/policy/liteqube.Error" "${VM_AUDIO} dom0 allow"
-add_line dom0 "/etc/qubes-rpc/policy/liteqube.SignalSound" "${VM_AUDIO} dom0 allow"
 add_line dom0 "/etc/qubes-rpc/policy/liteqube.SplitXorg" "${VM_AUDIO} ${VM_XORG} allow"
+add_line dom0 "/etc/qubes-rpc/policy/liteqube.SignalSound" "${VM_AUDIO} dom0 allow"
 add_line dom0 "/etc/qubes-rpc/policy/admin.Events" "${VM_AUDIO} "'$adminvm allow,target=$adminvm'
 add_line dom0 "/etc/qubes-rpc/policy/admin.vm.List" "${VM_AUDIO} "'$adminvm allow,target=$adminvm'
-add_line dom0 "/etc/qubes-rpc/policy/admin.vm.feature.CheckWithTemplate" "${VM_AUDIO} "'$adminvm allow,target=$adminvm'
 add_line dom0 "/etc/qubes-rpc/policy/admin.vm.property.Get" "${VM_AUDIO} "'$adminvm allow,target=$adminvm'
 add_line dom0 "/etc/qubes-rpc/policy/admin.vm.property.GetAll" "${VM_AUDIO} "'$adminvm allow,target=$adminvm'
 for VM in ${QUBES_WITH_SOUND} ; do
     add_line dom0 "/etc/qubes-rpc/policy/admin.Events" "${VM_AUDIO} ${VM} "'allow,target=$adminvm'
     add_line dom0 "/etc/qubes-rpc/policy/admin.vm.List" "${VM_AUDIO} ${VM} "'allow,target=$adminvm'
-    add_line dom0 "/etc/qubes-rpc/policy/admin.vm.feature.CheckWithTemplate" "${VM_AUDIO} ${VM} "'allow,target=$adminvm'
     add_line dom0 "/etc/qubes-rpc/policy/admin.vm.property.Get" "${VM_AUDIO} ${VM} "'allow,target=$adminvm'
     add_line dom0 "/etc/qubes-rpc/policy/admin.vm.property.GetAll" "${VM_AUDIO} ${VM} "'allow,target=$adminvm'
     vm_exists "${VM}" && qvm-prefs "${VM}" audiovm "${VM_AUDIO}"
